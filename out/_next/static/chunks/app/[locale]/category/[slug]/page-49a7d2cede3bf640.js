f85884d43bffe63e418522eb6766fae0d8fa1914
prot <prot@proton.me>
2026-09-19 12:10:46 +0300

    حفظ التعديلات المؤقتة

---
+++ out/_next/static/chunks/app/[locale]/category/[slug]/page-49a7d2cede3bf640.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2,824],{9696:function(n,e,u){Promise.resolve().then(u.bind(u,2789))}},function(n){n.O(0,[972,878,789,971,117,744],function(){return n(n.s=9696)}),_N_E=n.O()}]);
